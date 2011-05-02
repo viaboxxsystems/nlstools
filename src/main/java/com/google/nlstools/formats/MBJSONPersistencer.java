48fb5a33afdf67ba6903e714febbc4a72fae5350
roman.stumm <roman.stumm@e28af864-71ef-9762-cd9a-a6e5a303c4ec>
2011-05-02 14:00:43 +0000

    InMemoryPersistencer and copy() Method for all models

--- src/main/java/com/google/nlstools/formats/MBJSONPersistencer.java
+++ src/main/java/com/google/nlstools/formats/MBJSONPersistencer.java
@@ -2,21 +2,37 @@ package com.google.nlstools.formats;
 
 import com.google.nlstools.model.MBBundles;
 import com.google.nlstools.util.FileUtils;
-import com.sdicons.json.mapper.JSONMapper;
-import com.sdicons.json.model.JSONValue;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
+import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
 import java.io.Writer;
 
 /**
- * Description: <br/>
+ * Description: Load/Save JSON Format with XStream<br/>
  * User: roman.stumm <br/>
  * Date: 14.06.2007 <br/>
  * Time: 15:29:50 <br/>
  * Copyright: Viaboxx GmbH
  */
 public class MBJSONPersistencer extends MBPersistencer {
-    private final boolean pretty;
+    static final XStream xstream;
+    static final XStream xstream_pretty;
+
+    private boolean pretty;
+
+    static {
+        xstream = new XStream(new JettisonMappedXmlDriver());
+        xstream.setMode(XStream.NO_REFERENCES);
+        MBXMLPersistencer.configure(xstream);
+
+        xstream_pretty = new XStream(new JsonHierarchicalStreamDriver());
+        xstream_pretty.setMode(XStream.NO_REFERENCES);
+        MBXMLPersistencer.configure(xstream_pretty);
+    }
 
     public MBJSONPersistencer(boolean pretty) {
         this.pretty = pretty;
@@ -26,19 +42,26 @@ public class MBJSONPersistencer extends MBPersistencer {
         saveObject(object, file);
     }
 
-    public void saveObject(Object object, File file) throws Exception {
-        Writer writer = FileUtils.openFileWriterUTF8(file);
+    public void saveObject(Object obj, File target) throws Exception {
+        Writer out = FileUtils.openFileWriterUTF8(target);
         try {
-            JSONValue json = JSONMapper.toJSON(object);
-            writer.write(json.render(pretty));
+            if (pretty) {
+                xstream_pretty.toXML(obj, out);
+            } else {
+                xstream.toXML(obj, out);
+            }
         } finally {
-            writer.close();
+            out.close();
         }
     }
 
-    @Override
-    public MBBundles load(File source) throws Exception {
-        // TODO RSt - not yet implemented
-        return null;
+    public MBBundles load(File source) throws IOException, ClassNotFoundException {
+        Reader reader = FileUtils.openFileReaderUTF8(source);
+        try {
+            return (MBBundles) xstream.fromXML(reader);
+        } finally {
+            reader.close();
+        }
     }
+
 }
