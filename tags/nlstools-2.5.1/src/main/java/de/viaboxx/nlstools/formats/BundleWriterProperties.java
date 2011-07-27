dae069b6a29f9baf82474b617da7c87a0dfb3bfc
roman.stumm <roman.stumm@e28af864-71ef-9762-cd9a-a6e5a303c4ec>
2011-07-27 07:22:54 +0000

    [maven-release-plugin]  copy for tag nlstools-2.5.1

---
+++ tags/nlstools-2.5.1/src/main/java/de/viaboxx/nlstools/formats/BundleWriterProperties.java
@@ -0,0 +1,85 @@
+package de.viaboxx.nlstools.formats;
+
+import de.viaboxx.nlstools.model.MBBundle;
+import de.viaboxx.nlstools.util.FileUtils;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+
+import java.io.*;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Description: <br/>
+ * User: roman.stumm <br/>
+ * Date: 15.06.2007 <br/>
+ * Time: 09:44:05 <br/>
+ * Copyright: Viaboxx GmbH
+ */
+public class BundleWriterProperties extends BundleWriter {
+    /**
+     * charset (e.g. UTF-8) of the .properties Files - if they do not use the default charset (e.g. Grails i18n files)
+     * By default, the ISO 8859-1 character encoding is used (see javadoc of java.util.Properties)
+     */
+    private String charset = null;
+
+    public BundleWriterProperties(Task task, String configFile, MBBundle currentBundle, String outputPath,
+                                  FileType fileType, Set<String> allowedLocales) {
+        super(task, configFile, currentBundle, outputPath, fileType, allowedLocales);
+    }
+
+    public String getCharset() {
+        return charset;
+    }
+
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+
+    protected String suffix() {
+        return fileType == FileType.XML ? ".xml" : ".properties";
+    }
+
+    protected String getPropertiesHeader(String locale) {
+        return " THIS FILE HAS BEEN GENERATED AUTOMATICALLY - DO NOT ALTER!\r\n" +
+                "#\r\n" + "# resource bundle: " + getCurrentBundle().getBaseName() +
+                "\r\n" + "# locale: " + locale + "\r\n" + "# interface: " +
+                getCurrentBundle().getInterfaceName() + "\r\n" + "#";
+    }
+
+    protected void writeOutputFilePerLocale(String locale)
+            throws IOException {
+        String propfile = getFileName(locale);
+        task.log("writing resource file " + propfile, Project.MSG_INFO);
+        mkdirs(propfile);
+
+        if (getCharset() == null || fileType.equals(FileType.XML)) {
+            FileOutputStream stream = new FileOutputStream(propfile);
+            try {
+                String header = getPropertiesHeader(locale);
+                writeProperties(stream, locale, header);
+            } finally {
+                stream.close();
+            }
+        } else {
+            Writer writer = FileUtils.openFileWriter(new File(propfile), getCharset());
+            try {
+                String header = getPropertiesHeader(locale);
+                Properties p = createProperties(locale);
+                p.store(writer, header);
+            } finally {
+                writer.close();
+            }
+        }
+    }
+
+    protected void writeProperties(OutputStream stream, String aLocale, String header)
+            throws IOException {
+        Properties p = createProperties(aLocale);
+        if (fileType.equals(FileType.XML)) {
+            p.storeToXML(stream, header);
+        } else {
+            p.store(stream, header);
+        }
+    }
+}
---
+++ tags/nlstools-2.5.1/src/main/java/de/viaboxx/nlstools/model/MBEntry.java
@@ -0,0 +1,114 @@
+package de.viaboxx.nlstools.model;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Description: <br/>
+ * User: roman.stumm <br/>
+ * Date: 14.06.2007 <br/>
+ * Time: 15:21:57 <br/>
+ * Copyright: Viaboxx GmbH
+ */
+@XStreamAlias("entry")
+public class MBEntry implements Comparable, Cloneable {
+    @XStreamAsAttribute
+    private String key;
+    private String description;  // comment field
+    @XStreamImplicit(itemFieldName = "text")
+    private List<MBText> texts = new ArrayList<MBText>();
+
+    public void sort() {
+        if (texts != null) Collections.sort(texts);
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<MBText> getTexts() {
+        if (texts == null) texts = new ArrayList<MBText>();
+        return texts;
+    }
+
+    public void setTexts(List<MBText> texts) {
+        this.texts = texts;
+    }
+
+    public MBText getText(String locale) {
+        for (MBText text : getTexts()) {
+            if (locale != null && locale.equals(text.getLocale())) {
+                return text;
+            }
+        }
+        return null;
+    }
+
+    public boolean isReview(String reviewLocale) {
+        if (texts == null || texts.isEmpty()) return true;
+        if (reviewLocale == null) {
+            for (MBText each : texts) {
+                if (each.isReview() ||
+                        (!each.isUseDefault() && StringUtils.isEmpty(each.getValue())))
+                    return true;
+            }
+            return false;
+        } else {
+            MBText text = getText(reviewLocale);
+            return (text == null || text.isReview() ||
+                    (!text.isUseDefault() && StringUtils.isEmpty(text.getValue())));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MBEntry mbEntry = (MBEntry) o;
+        return !(description != null ? !description.equals(mbEntry.description) : mbEntry.description != null) &&
+                !(key != null ? !key.equals(mbEntry.key) : mbEntry.key != null) &&
+                !(texts != null ? !texts.equals(mbEntry.texts) : mbEntry.texts != null);
+    }
+
+    public int compareTo(Object o) {
+        if (o instanceof MBEntry) {
+            if (getKey() != null) {
+                return getKey().compareTo(((MBEntry) o).getKey());
+            } else {
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    public MBEntry copy() {
+        try {
+            MBEntry entry = (MBEntry) clone();
+            entry.setTexts(new ArrayList<MBText>(getTexts().size()));
+            for (MBText text : getTexts()) {
+                entry.getTexts().add(text.copy());
+            }
+            return entry;
+        } catch (CloneNotSupportedException ex) {
+            return null;
+        }
+    }
+}
